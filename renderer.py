d91b6734f777b9fc180749bd0f637eea386d1255
Mohammed Asim Hussain <mohdasimhussain0010@gmail.com>
2026-01-13 13:13:53 +0300

    created renderer.py file

---
+++ renderer.py
@@ -0,0 +1,13 @@
+# Mohammed Asim Hussain, 2026
+
+import pygame
+
+from sys import exit
+from game import MineMind
+from ai import AI
+
+
+def main():
+   # Define height and width of the grid we need to render
+   BOARD_ HEIGHT = 16
+   
